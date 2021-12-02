d859b3b7232c40d6aa636c7c9debcba0ad19477b
xue <1943235416@qq.com>
2021-12-02 19:27:35 +0800

    first commit

---
+++ src/main/java/com/test/git/GitTest.java
@@ -0,0 +1,18 @@
+package com.test.git;
+
+/**
+ * @author XueWe
+ * @title: GitTest
+ * @projectName git
+ * @description: TODO
+ * @date 2021/12/218:57
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+    public String test(int i){
+        return "test";
+    }
+}
